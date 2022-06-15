af4ae4eb09b8bae407772a54fd671ad48a466bc7
zaroursamy <samy.zarour8430@gmail.com>
2022-06-15 22:24:33 +0200

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Run program
+
+Run this command line, replacing `KEY_ID` by your API key id. In order to get a key id, you have to register to https://developer.aife.economie.gouv.fr/ by creating an account and get an access to the Judilibre API.
+````bash
+cargo run -- KEY_ID
+````
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use api_model::JudiResponse;
 use reqwest::header::{ACCEPT, CONTENT_TYPE};
 use reqwest::Client;
+use std::env;
 mod api_model;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+    let arguments: Vec<String> = env::args().collect();
+    let key_id = arguments[1].trim();
+
     let base_export_url = "https://sandbox-api.piste.gouv.fr/cassation/judilibre/v1.0/export?";
     let date_start = "2022-01-01";
     let date_end = "2022-05-30";
     let batch_size = 2;
     let batch = 0;
 
-    let key_id = "68e791c6-974b-4522-a8f9-dd4bb6a5ea68";
     let url = format!("{base_export_url}batch={batch}&batch_size={batch_size}&date_start={date_start}&date_end={date_end}");
 
     println!("{:?}", &url);
@@ -28,7 +31,7 @@ async fn main() -> Result<(), reqwest::Error> {
         .json()
         .await?;
 
-    println!("Success! {:#?}", response);
+    println!("{:#?}", response);
 
     Ok(())
 }
